b17bef052479adb2a64b375318a677ca7601ef65
Marco <marco@mojica.de>
2019-05-30 21:27:06 +0200

    Test camera movement

--- src/index.js
+++ src/index.js
@@ -1,59 +1,75 @@
-import { Engine, Events, Render, World, Bodies, Bounds } from "matter-js"
+import { Bodies, Body, Bounds, Engine, Events, Render, Runner, World } from "matter-js"
 
 export default container => {
     // create an engine
-    const engine = Engine.create({})
+    const engine = Engine.create()
+    engine.world.gravity = { x: 0, y: 0.01 }
+
+    // create a runner
+    const runner = Runner.create()
+
+    //run the engine
+    Runner.run(runner, engine)
     
     // create a renderer
     const render = Render.create({
         element: container,
         engine: engine,
         options: {
-            width: 800,
-            height: 600
+            width: container.clientWidth,
+            height: 1000,
+            hasBounds: true
         }
     })
-
-    engine.world.gravity = { x: 0, y: 0 }
-
-    const ground = Bodies.rectangle(400, 0, 800, 30, {
-        isStatic: true,
-        label: "ground"
-    })
-
-    // World.add(engine.world, ground)
-
-    // run the engine
-    Engine.run(engine)
 
     // run the renderer
     Render.run(render)
 
+    const width = render.canvas.width
+    const height = render.canvas.height
+
+    const ground = Bodies.rectangle(width / 2, height - 30, width * 2, 1, {
+        isStatic: true,
+        label: "ground",
+        collisionFilter: {
+            category: 1,
+            mask: 2
+        }
+    })
+
+    World.add(engine.world, ground)
+
     setInterval(() => {
-        const fan = 30
-        const spin = 5
+        const fan = 3
+        const speed = 0.7
 
         const angle = (180 + fan / 2 - Math.random() * fan) * Math.PI / 180
-        const x = Math.sin(angle)
-        const y = Math.cos(angle)
+        const x = Math.sin(angle) * speed
+        const y = Math.cos(angle) * speed
 
-        const torque = Math.random() * spin - spin / 2
+        const spread = 10
+        const xPos = width / spread + (width / spread) * (spread - 2) * Math.random() 
 
-        const xPos = 200 + 400 * Math.random() 
-
-        const box = Bodies.rectangle(xPos, 600, 20, 20, {
+        const box = Bodies.rectangle(xPos, ground.position.y - 30, 20, 20, {
+            frictionAir: 0.004,
             force: { x, y },
-            density: 0.1,
-            label: "box"
+            density: 0.05,
+            label: "box",
+            collisionFilter: {
+                category: 2,
+                mask: 1
+            }
         })
 
         World.add(engine.world, box)
+    }, 300)
 
-        const translate = {x : 0, y : -100}
+    Events.on(runner, "afterTick", () => {
+        const translate = {x : 0, y : -0.1 * runner.delta}
         Bounds.translate(render.bounds, translate)
-    }, 30)
+        Body.translate(ground, translate)
+    })
 
-    /*
     Events.on(engine, "collisionStart", ({ pairs }) => {
         pairs.forEach(({ bodyA, bodyB }) => {
             if(bodyA.label === "ground" || bodyB.label === "ground"){
@@ -65,5 +81,4 @@ export default container => {
             }
         })
     })
-    */
 }

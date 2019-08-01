b2feaf0dac9fb81cf5c62450a2bb3f3cd696bb4c
Marco <marco@mojica.de>
2019-08-01 22:41:42 +0200

    Use next-offline

--- next.config.js
+++ next.config.js
@@ -1,3 +1,30 @@
-module.exports = {
-  target: 'serverless'
+const withOffline = require("next-offline")
+
+const nextConfig = {
+  target: 'serverless',
+  transformManifest: manifest => ['/'].concat(manifest),
+  workboxOpts: {
+    generateInDevMode: true,
+    swDest: 'static/service-worker.js',
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'https-calls',
+          networkTimeoutSeconds: 15,
+          expiration: {
+            maxEntries: 150,
+            maxAgeSeconds: 30 * 24 * 60 * 60,
+          },
+          cacheableResponse: {
+            statuses: [0, 200],
+          },
+        },
+      },
+    ],
+  },
+
 }
+
+module.exports = withOffline(nextConfig)

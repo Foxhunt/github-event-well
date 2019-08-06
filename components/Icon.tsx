09a4235c17ff089038f4f0b3a1b2b0264c332676
Marco <marco@mojica.de>
2019-08-06 23:20:22 +0200

    Cleanup

--- components/Icon.tsx
+++ components/Icon.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from "react"
+import { useEffect, useRef } from "react"
 import Octicon from "@primer/octicons-react"
 
 import { Body, Constraint, World } from "matter-js"

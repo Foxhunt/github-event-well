c886d1953edf0b9f5b231771d4fb5a87191231f0
Marco <marco@mojica.de>
2019-08-21 01:02:16 +0200

    Add tutorial text to ui layer

--- components/UserCard.tsx
+++ components/UserCard.tsx
@@ -1,16 +1,16 @@
 import { useContext, useState, useEffect } from "react"
-import { AnimatePresence, motion } from "framer-motion"
+import { motion } from "framer-motion"
 import Octicon, { getIconByName } from "@primer/octicons-react"
 
 import { BackgroundContext } from "../src/backgroundContext"
 
 export default function UserCard({ event, position }) {
-    const background = useContext(BackgroundContext)
+    const background = useContext<HTMLDivElement>(BackgroundContext)
 
     const [open, setOpen] = useState(false)
 
-    const [x, setX] = useState(0)
-    const [y, setY] = useState(0)
+    const [x, setX] = useState(Math.random() * background.clientWidth)
+    const [y, setY] = useState(Math.random() * background.clientHeight)
 
     const width = 200
     const height = open ? 125 : 40
@@ -37,79 +37,75 @@ export default function UserCard({ event, position }) {
     }, [position, position.x, position.y, background, height])
 
     return <>
-        <AnimatePresence>
-            {
-                event &&
-                <motion.div
-                    style={{
-                        backgroundColor: "rgba(0, 0, 0, 0.5)",
-                        backdropFilter: "blur(1px)",
-                        borderRadius: 20,
-                        height,
-                        width: 40,
-                        display: "grid",
-                        gridTemplateColumns: "40px auto 25px",
-                        gridTemplateRows: "40px auto",
-                        overflow: "hidden",
-                        pointerEvents: "all"
-                    }}
-                    initial={{
-                        x,
-                        y,
-                        width: 40,
-                        opacity: 0
-                    }}
-                    animate={{
-                        x,
-                        y,
-                        width,
-                        height,
-                        opacity: 1
-                    }}
-                    exit={{
-                        x,
-                        y,
-                        width: 40,
-                        height: 40,
-                        opacity: 0
-                    }}
-                    transition={{
-                        type: "spring",
-                        damping: 20,
-                        mass: 1,
-                        stiffness: 200,
-                        velocity: 2
-                    }}>
-                    <img
-                        onClick={() => {
-                            event && window.open(`https://github.com/${event.actor.login}`)
-                        }}
-                        src={event.actor.avatar_url} />
-                    <div
-                        id="repo"
-                        onClick={() => {
-                            event && window.open(`https://github.com/${event.repo.name}`)
-                        }}>{
-                            event.repo.name
-                        }</div>
-                    <div
-                        id="openCloseDetails"
-                        onClick={() => {
-                            setOpen(open => !open)
-                        }}>
-                        <Octicon
-                            icon={getIconByName(open ? "chevron-up" : "chevron-down")}
-                            size={"small"} />
-                    </div>
-                    <div
-                        id="details">
-                        actor: {event.actor.display_login} <br />
-                        time: {new Date(Date.parse(event.created_at)).toLocaleString()} <br />
-                        type: {event.type} <br />
-                    </div>
-                </motion.div>
-            }
-        </AnimatePresence>
+        <motion.div
+            style={{
+                position: "absolute",
+                backgroundColor: "rgba(0, 0, 0, 0.5)",
+                backdropFilter: "blur(1px)",
+                borderRadius: 20,
+                height,
+                width: 40,
+                display: "grid",
+                gridTemplateColumns: "40px auto 25px",
+                gridTemplateRows: "40px auto",
+                overflow: "hidden",
+                pointerEvents: "all"
+            }}
+            initial={{
+                x,
+                y,
+                width: 40,
+                opacity: 0
+            }}
+            animate={{
+                x,
+                y,
+                width,
+                height,
+                opacity: 1
+            }}
+            exit={{
+                x,
+                y,
+                width: 40,
+                height: 40,
+                opacity: 0
+            }}
+            transition={{
+                type: "spring",
+                damping: 20,
+                mass: 1,
+                stiffness: 200,
+                velocity: 2
+            }}>
+            <img
+                onClick={() => {
+                    event && window.open(`https://github.com/${event.actor.login}`)
+                }}
+                src={event.actor.avatar_url} />
+            <div
+                id="repo"
+                onClick={() => {
+                    event && window.open(`https://github.com/${event.repo.name}`)
+                }}>{
+                    event.repo.name
+                }</div>
+            <div
+                id="openCloseDetails"
+                onClick={() => {
+                    setOpen(open => !open)
+                }}>
+                <Octicon
+                    icon={getIconByName(open ? "chevron-up" : "chevron-down")}
+                    size={"small"} />
+            </div>
+            <div
+                id="details">
+                actor: {event.actor.display_login} <br />
+                time: {new Date(Date.parse(event.created_at)).toLocaleString()} <br />
+                type: {event.type} <br />
+            </div>
+        </motion.div>
         <style jsx>{`
             img {
                 height: 100%;
@@ -121,19 +117,13 @@ export default function UserCard({ event, position }) {
             }
             #repo {
                 padding-left: 5px;
-                
-                font-family: "Roboto", sans-serif;
-                font-style: normal;
-                font-weight: normal;
+
                 font-size: 10px;
                 line-height: 40px;
                 text-transform: capitalize;
 
                 overflow: hidden;
-                white-space: nowrap;
                 text-overflow: ellipsis;
-
-                color: #FFFFFF;
             }
             #openCloseDetails {
                 color: ${open ? "white" : "#696969"};
@@ -144,17 +134,12 @@ export default function UserCard({ event, position }) {
             #details {
                 grid-column: 1/-1;
                 grid-row: 2/-1;
-                color: #FFFFFF;
 
                 padding: 8px;
-                
-                font-family: "Roboto", sans-serif;
-                font-style: normal;
-                font-weight: normal;
+
                 font-size: 14px;
 
                 overflow: hidden;
-                white-space: nowrap;
                 text-overflow: ellipsis;
             }
         `}</style>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,8 @@
 import { useRef, useState } from "react"
 import Head from "next/head"
+import { motion, AnimatePresence } from "framer-motion"
+
+import useLocalStorage from "../src/useLocalStorage"
 
 import { EngineContext } from "../src/engineContext"
 import { BackgroundContext } from "../src/backgroundContext"
@@ -17,14 +20,15 @@ export default function Index() {
     const backgroundRef = useRef()
 
     const [selectedEvent, setSelectedEvent] = useState(null)
-    const [userCardPosition, setUserCardPosition] = useState({x: 0, y: 0})
+    const [userCardPosition, setUserCardPosition] = useState({ x: 0, y: 0 })
+    const [tutorial, setTutorial] = useLocalStorage("tutorial", true)
 
     return <>
         <Head>
             <title>GitHub Events</title>
             <meta
                 name="viewport"
-                content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui"/>
+                content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui" />
             <link rel="manifest" href="/static/manifest.json" />
         </Head>
         <EngineContext.Provider value={engine}>
@@ -33,7 +37,7 @@ export default function Index() {
                     ref={backgroundRef}
                     className={"background"}
                     onClickCapture={event => {
-                        if(event.target === backgroundRef.current){
+                        if (event.target === backgroundRef.current) {
                             setSelectedEvent(null)
                         }
                     }}>
@@ -42,10 +46,11 @@ export default function Index() {
                             <Icon
                                 key={event.id}
                                 event={event}
-                                selected={ event === selectedEvent}
-                                selectEvent={ position => {
+                                selected={event === selectedEvent}
+                                selectEvent={position => {
                                     setUserCardPosition(position)
                                     setSelectedEvent(event)
+                                    setTutorial(false)
                                 }}
                                 remove={() => {
                                     setEvents(events => events.filter(e => e !== event))
@@ -55,31 +60,62 @@ export default function Index() {
                 </div>
                 <div
                     className={"uiLayer"}>
-                    <UserCard
+                    <AnimatePresence>
+                        {selectedEvent && <UserCard
+                            key={"userCard"}
                             position={userCardPosition}
-                            event={selectedEvent} />
+                            event={selectedEvent} />}
+                        {tutorial && <motion.div
+                            key={"tutorial"}
+                            initial={{
+                                position: "absolute",
+                                bottom: "0px",
+                                width: "100vw",
+                                height: "20vh",
+                                textAlign: "center",
+                                opacity: 0,
+                            }}
+                            animate={{
+                                opacity: 1
+                            }}
+                            exit={{
+                                opacity: 0
+                            }}>{
+                                "tap an icon"
+                            }</motion.div>}
+                    </AnimatePresence>
                 </div>
                 <style jsx>{`
                     .background {
-                        position: relative;
+                        position: absolute;
                         overflow: hidden;
-                        width: calc(100vw - 16px);
-                        height: calc(100vh - 16px);
+                        width: 100vw;
+                        height: 100vh;
                         background-color: #272727;
                     }
                     .uiLayer {
                         pointer-events: none;
                         position: absolute;
-                        top: 8px;
                         overflow: hidden;
-                        width: calc(100vw - 16px);
-                        height: calc(100vh - 16px);
+                        width: 100vw;
+                        height: 100vh;
                     }
                 `}</style>
                 <style jsx global>{`
                     @import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
                     html {
                         user-select: none;
+                
+                        font-family: "Roboto", sans-serif;
+                        font-style: normal;
+                        font-weight: normal;
+
+                        white-space: nowrap;
+
+                        color: #FFFFFF;
+                    }
+                    body {
+                        margin: 0px;
                     }
                 `}</style>
             </BackgroundContext.Provider>
